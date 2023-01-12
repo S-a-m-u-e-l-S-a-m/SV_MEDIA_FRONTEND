e91f8fd3ebf57c2e6167df1ec51647f1328aa0fe
S-a-m-u-e-l-S-a-m <samuel126895@gmail.com>
2023-01-12 21:55:36 +0530

    first commit

---
+++ src/Data/TrendData.js
@@ -0,0 +1,27 @@
+export const TrendData= [
+    {
+      name: "Minions",
+      shares: 87,
+    },
+    {
+      name: "Avengers",
+      shares: 80.5,
+    },
+    {
+      name: "Samuel",
+      shares: 85.5,
+    },
+    {
+      name: "Reactjs",
+      shares: 72,
+    },
+    {
+      name: "NullClass",
+      shares: 71.9,
+    },
+    {
+      name: "YouGetPlaced",
+      shares: 70,
+    },
+  ];
+  
